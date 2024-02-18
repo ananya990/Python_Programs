35376b95f1bd159bbaa86c8bec976b0fddac98f1
ananya990 <142164935+ananya990@users.noreply.github.com>
2024-02-18 17:20:19 +0530

    Create Python_Program_to_Swap_Two_Variables_Using_Multiplication_and_Division.py

---
+++ Python_Program_to_Swap_Two_Variables_Using_Multiplication_and_Division.py
@@ -0,0 +1,9 @@
+x = float(input("Enter value of x : "))
+y = float(input("Enter value of y : "))
+
+x = x * y
+y = x / y
+x = x / y
+
+print("The value of x after swapping : ", x)
+print("The value of y after swapping : ", y)
