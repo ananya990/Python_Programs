a52518708c1947ab29f6912f254d638cc58b71b6
ananya990 <142164935+ananya990@users.noreply.github.com>
2024-02-25 15:18:15 +0530

    Create Python_Program_to_Find_LCM_Using_GCD.py

---
+++ Python_Program_to_Find_LCM_Using_GCD.py
@@ -0,0 +1,13 @@
+def compute_gcd(x, y):
+    while(y):
+       x, y = y, x % y
+   return x
+
+def compute_lcm(x, y):
+   lcm = (x*y)//compute_gcd(x,y)
+   return lcm
+
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : ")) 
+
+print("The L.C.M. of",num1,"and",num2,"is", compute_lcm(num1, num2))
