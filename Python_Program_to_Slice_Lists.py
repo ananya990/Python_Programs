43b4a00b35f65bd33228b1df56a79793fec3293d
ananya990 <142164935+ananya990@users.noreply.github.com>
2024-03-03 13:27:42 +0530

    Create Python_Program_to_Slice_Lists.py
    The slice() function is a built-in function that creates a slice object representing the set of indices specified by range(start, stop, step). 
    It's commonly used to extract a portion of a sequence (such as a list, tuple, or string) without modifying the original sequence.
    
    Syntax :
                 slice(start, stop, step)
    - start (optional): The starting index of the slice(included). If not provided, it defaults to None, indicating the beginning of the sequence.
    - stop: The stopping index of the slice. If not provided, it defaults  goes up to end of the sequence, but does not include this index.
    - step (optional): Select nth item within the range start to stop. If not provided, it defaults to None, indicating a step of 1.

---
+++ Python_Program_to_Slice_Lists.py
@@ -0,0 +1,19 @@
+my_list = [10, 20, 30, 40, 50]
+
+# Get all the Items
+print(my_list[:])
+
+# Get all the Items After a Specific Position
+print(my_list[2:])
+
+# Get all the Items Before a Specific Position
+print(my_list[:2])
+
+# Get all the Items from One Position to Another Position
+print(my_list[2:4])
+
+# Get the Items at Specified Intervals
+print(my_list[::2])
+
+# Get the Items from last at Specified Intervals
+print(my_list[::-2])
