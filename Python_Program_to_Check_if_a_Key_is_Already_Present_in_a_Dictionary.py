8572cfc16a97f32b2469ebc38882e9489b4c5c8d
ananya990 <142164935+ananya990@users.noreply.github.com>
2024-03-07 16:38:08 +0530

    Create Python_Program_to_Check_if_a_Key_is_Already_Present_in_a_Dictionary.py

---
+++ Python_Program_to_Check_if_a_Key_is_Already_Present_in_a_Dictionary.py
@@ -0,0 +1,9 @@
+my_dict = {1:'a', 2:'b', 3:'c', 4:'d'}
+
+k=int(input("Enter a key you want to search : "))
+
+if k in my_dict:
+    print("This key is present")
+
+else:
+  print("This key is absent")
