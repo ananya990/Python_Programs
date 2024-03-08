51b508b01edf56324b3b8206086c6ed3d621a850
ananya990 <142164935+ananya990@users.noreply.github.com>
2024-03-08 11:19:09 +0530

    Create Python_Program_to_Print_Output_Without_a_Newline.py
     By default, the print function in Python appends a newline character ('\n') at the end of the printed content, moving the cursor to the next line, but the end argument lets you customize this behavior. The end keyword is often used as an argument when printing output to control the character that is printed at the end of the line.
    
    The end parameter specifies the string that will be printed at the end of the print statement. It can be any string or an empty string (' ') if you don't want to add anything specific.

---
+++ Python_Program_to_Print_Output_Without_a_Newline.py
@@ -0,0 +1,15 @@
+# Default behavior: newline at the end
+print("Hello")
+print("World")
+# Output:
+# Hello
+# World
+
+# new line
+print()
+
+# Using the end parameter to print on the same line
+print("Hello", end=" ")
+print("World")
+# Output:
+# Hello World
