37960988aa58d80e32a01333c069ac157cfd8ebb
ananya990 <142164935+ananya990@users.noreply.github.com>
2024-03-18 00:18:46 +0530

    Create Python_Program_to_Find_the_Size_(Resolution)_of_an_Image.py

---
+++ Python_Program_to_Find_the_Size_(Resolution)_of_an_Image.py
@@ -0,0 +1,25 @@
+def jpeg_res(filename):
+   # open image for reading in binary mode
+   with open(filename,'rb') as img_file:
+
+       # height of image (in 2 bytes) is at 164th position
+       img_file.seek(163)
+
+       # read the 2 bytes
+       a = img_file.read(2)
+
+       # calculate height
+       height = (a[0] << 8) + a[1]
+
+       # next 2 bytes is width
+       a = img_file.read(2)
+
+       # calculate width
+       width = (a[0] << 8) + a[1]
+
+   print("The resolution of the image is",width,"x",height)
+  
+
+# This will print the resolution of the image named "img1.jpg". 
+# Make sure to replace "img1.jpg" with the filename of your JPEG image.
+jpeg_res("img1.jpg")
