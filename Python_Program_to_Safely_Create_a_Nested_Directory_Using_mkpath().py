b0acd981c2be9cdb04ec39b960532858930723fa
ananya990 <142164935+ananya990@users.noreply.github.com>
2024-03-23 22:39:29 +0530

    Create Python_Program_to_Safely_Create_a_Nested_Directory_Using_mkpath().py

---
+++ Python_Program_to_Safely_Create_a_Nested_Directory_Using_mkpath().py
@@ -0,0 +1,6 @@
+# Import the module 'distutils.dir_util' which provides functions for working with directories.
+import distutils.dir_util
+
+# Call the function 'mkpath' from the 'distutils.dir_util' module to create a directory path.
+# The directory path "/root/dirA/dirB" will be created.
+distutils.dir_util.mkpath("/root/dirA/dirB")
